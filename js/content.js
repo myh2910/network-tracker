edbb87587f4059c2fc81968c082ad3ebc97ab156
Yohan Min <minleeyohan@gmail.com>
2022-07-25 03:34:32 -0500

    Improve readability

--- js/content.js
+++ js/content.js
@@ -1,12 +1,10 @@
 let requests = [];
 
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		if ((request.from === 'background') && (request.subject === 'requestInfo')) {
-			let details = request.content;
-			requests.push(details);
-		} else if ((request.from === 'popup') && (request.subject === 'DOMInfo')) {
-			sendResponse(requests);
-		}
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	if ((request.from === 'background') && (request.subject === 'requestInfo')) {
+		let details = request.content;
+		requests.push(details);
+	} else if ((request.from === 'popup') && (request.subject === 'DOMInfo')) {
+		sendResponse(requests);
 	}
-);
+});
