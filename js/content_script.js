aa978885e51b7b22308d0b7f9b45b029c9ea48b4
Yohan Min <minleeyohan@gmail.com>
2022-11-21 07:45:27 -0500

    Receive JSON and convert it to blob

--- js/content_script.js
+++ js/content_script.js
@@ -1,48 +1,49 @@
+let arrayBuffers = {};
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	if (message.to === 'content_script') {
-		if (message.from === 'background' && message.subject === 'download_playlist') {
-			downloadPlaylist(message.data);
-			chrome.runtime.sendMessage({
-				from: 'content_script',
-				to: 'popup',
-				subject: 'update_status',
-				data: {idx: message.data.idx, msg: message.data.url}
-			});
-		} else if (message.from === 'background' && message.subject === 'alert') {
+		if (message.from === 'background' && message.subject === 'alert') {
 			alert(message.data);
+		} else if (message.from === 'background' && message.subject === 'download_playlist') {
+			downloadPlaylist(message.data);
 		}
 	}
 });
 
-async function downloadPlaylist(data) {
-	if (data.urls.length === 0) {
-		alert(`Error: Cannot download ${data.url}`);
-		return false;
-	}
-	let blobs = [];
-	for (const [idx, url] of data.urls.entries()) {
-		chrome.runtime.sendMessage({
-			from: 'content_script',
-			to: 'popup',
-			subject: 'update_status',
-			data: {idx: data.idx, msg: `[${idx + 1}/${data.urls.length}] ${url}`}
-		});
-		try {
-			const _res = await fetch(url);
-			const _blob = await _res.blob();
-			blobs.push(_blob);
-		} catch (err) {
-			alert(`[${idx + 1}/${data.urls.length}] Error: ${err}`);
-			return false;
+function downloadPlaylist(data) {
+	try {
+		let i = 0;
+		const array = new Uint32Array(data.arrayLength).fill().map(() => data.array[i++]);
+		if (data.urlsIdx === 0) {
+			arrayBuffers[data.requestId] = [];
+		} if (arrayBuffers[data.requestId]) {
+			arrayBuffers[data.requestId].push(array.buffer);
 		}
+		if (data.urlsIdx + 1 === data.urlsLength) {
+			if (arrayBuffers[data.requestId].length !== data.urlsLength) {
+				throw `Cannot download ${data.url}`;
+			}
+			const blob = new Blob(arrayBuffers[data.requestId], {type: 'video/mp4'});
+			chrome.runtime.sendMessage({
+					from: 'content_script',
+					to: 'background',
+					subject: 'download_playlist',
+					data: URL.createObjectURL(blob)
+				}, url => {
+					URL.revokeObjectURL(url);
+			});
+			chrome.runtime.sendMessage({
+				from: 'content_script',
+				to: 'popup',
+				subject: 'update_status',
+				data: {
+					tabId: data.tabId,
+					idx: data.idx,
+					msg: data.url
+				}
+			}, () => chrome.runtime.lastError);
+		}
+	} catch (err) {
+		alert(`Error: ${err}`);
 	}
-	const blob = new Blob(blobs, {type: 'video/mp4'});
-	chrome.runtime.sendMessage({
-			from: 'content_script',
-			to: 'background',
-			subject: 'download_playlist',
-			data: URL.createObjectURL(blob)
-		}, url => {
-			URL.revokeObjectURL(url);
-	});
 }
